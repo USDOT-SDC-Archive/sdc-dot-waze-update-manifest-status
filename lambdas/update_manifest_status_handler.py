74c68e2f7326afdfb77b4b32a46e69baa3d515e0
HannahKent <hkent@acrotron.com>
2019-09-23 15:34:57 -0600

    Reduced code smells

--- lambdas/update_manifest_status_handler.py
+++ lambdas/update_manifest_status_handler.py
@@ -2,9 +2,10 @@ import boto3
 from common.logger_utility import *
 from boto3.dynamodb.conditions import Attr, Key
 
+
 class ManifestHandler:
 
-    def update_manifest_status(self, event, context):
+    def update_manifest_status(self, event):
         batch_id = ""
         table_name = ""
         try:
@@ -13,28 +14,30 @@ class ManifestHandler:
             ddb_table_name = os.environ['DDB_MANIFEST_TABLE_ARN'].split('/')[1]
             manifest_index_name = os.environ['DDB_MANIFEST_FILES_INDEX_NAME']
             table_name = event['tablename']
-            batch_id=event['batchId']
+            batch_id = event['batchId']
             ddb_table = ddb.Table(ddb_table_name)
-            response = ddb_table.query(IndexName=manifest_index_name,
-                                       KeyConditionExpression=Key('BatchId'
-                                       ).eq(batch_id) & Key('TableName'
-                                       ).eq(table_name),
-                                       FilterExpression=Attr('FileStatus'
-                                       ).eq('open'))
-        
+            response = ddb_table.query(
+                IndexName=manifest_index_name,
+                KeyConditionExpression=Key('BatchId').eq(batch_id) & Key('TableName').eq(table_name),
+                FilterExpression=Attr('FileStatus').eq('open')
+            )
+
             if response['Count'] > 0:
                 for item in response['Items']:
-                    tableName = item['TableName']
-                    if tableName == table_name:
-                        response = ddb_table.update_item(Key={'ManifestId': item['ManifestId'
-                                ], 'BatchId': batch_id},
-                                UpdateExpression='set FileStatus = :f',
-                                ExpressionAttributeValues={':f': 'completed'})
-                        LoggerUtility.logError("Updated manifest status for  batchId {} and table {}".format(batch_id,table_name))
+                    table_name = item['TableName']
+                    if table_name == table_name:
+                        ddb_table.update_item(
+                            Key={'ManifestId': item['ManifestId'], 'BatchId': batch_id},
+                            UpdateExpression='set FileStatus = :f',
+                            ExpressionAttributeValues={':f': 'completed'}
+                        )
+                        LoggerUtility.log_error(
+                            "Updated manifest status for  batchId {} and table {}".format(batch_id, table_name))
                         break
         except Exception as e:
-            LoggerUtility.logError("Unable to update manifest status for  batchId {} and table {}".format(batch_id,table_name))
+            LoggerUtility.log_error(
+                "Unable to update manifest status for  batchId {} and table {}".format(batch_id, table_name))
             raise e
-    
-    def update_manifest(self, event, context):
-        self.update_manifest_status(event, context)
+
+    def update_manifest(self, event, *args, **kwargs):
+        self.update_manifest_status(event)
